95298580140e7d591a7e3efbcdda4914de6ea51a
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-10-22 18:13:54 -0300

    feat: task text

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -28,6 +28,7 @@ export function TodoInput({ addTask }: TodoInputProps) {
         //TODO - use value, onChangeText and onSubmitEditing props
         onChangeText={setTask}
         value={task}
+        onSubmitEditing={handleAddNewTask}
       />
       <TouchableOpacity
         testID="add-new-task-button"
